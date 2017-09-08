507a7d54fb05d3327bd419ae99bd4234afa7cff4
whuizenga <will.huizenga@gmail.com>
2017-09-08 15:23:03 -0400

    setup first homepage component

---
+++ client/src/Components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                This is the homepage
+            </div>
+        );
+    }
+}
+
+export default HomePage;
