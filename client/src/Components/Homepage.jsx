ab02b4d91f7920ff813d58447a147527fb911a0b
whuizenga <will.huizenga@gmail.com>
2017-09-08 15:24:41 -0400

    made gameplan for homepage

--- client/src/Components/Homepage.jsx
+++ client/src/Components/Homepage.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class HomePage extends Component {
     render() {
         return (
             <div>
-                This is the homepage
+                Make a button for signin-it goes in the upperleft corner
+
+                Make a button that grows into the login screen
+
+                Make a footer that has a link to the about page
             </div>
         );
     }
