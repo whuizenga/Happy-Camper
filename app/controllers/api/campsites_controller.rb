f1c45b303e4482d4664a5408641c6ea466b098fd
whuizenga <will.huizenga@gmail.com>
2017-09-10 10:55:25 -0400

    added call to get all nearby campsites

--- app/controllers/api/campsites_controller.rb
+++ app/controllers/api/campsites_controller.rb
@@ -3,13 +3,8 @@ class Api::CampsitesController < ApplicationController
     def index
         lat = params[:lat]
         long = params[:long]
-        puts lat
-        puts long
-        render json: {
-            "message": "success?"
-        }
-        # http://api.amp.active.com/camping/campgrounds?landmarkName=true&landmarkLat=33.77396&landmarkLong=-84.377415&xml=true&api_key=pe95bp8bdx9mk3d4ycgydg9v
-        # @xml = HTTParty.get("http://api.amp.active.com/camping/campgrounds?pstate=GA&api_key=#{ENV['CAMPGROUNDKEY']}")
-        # response = Hash.from_xml(@xml.body) 
+        @xml = HTTParty.get("http://api.amp.active.com/camping/campgrounds?landmarkName=true&landmarkLat=#{lat}&landmarkLong=#{long}$api_key=#{ENV['CAMPGROUNDKEY']}")
+        response = Hash.from_xml(@xml.body) 
+        render json: response
     end
 end
