6d399ba32ed8cc94ca8ca9fa99e6fb60e9260a9f
whuizenga <will.huizenga@gmail.com>
2017-09-10 11:03:45 -0400

    successfully load env into rails

--- app/controllers/api/campsites_controller.rb
+++ app/controllers/api/campsites_controller.rb
@@ -3,7 +3,7 @@ class Api::CampsitesController < ApplicationController
     def index
         lat = params[:lat]
         long = params[:long]
-        @xml = HTTParty.get("http://api.amp.active.com/camping/campgrounds?landmarkName=true&landmarkLat=#{lat}&landmarkLong=#{long}$api_key=#{ENV['CAMPGROUNDKEY']}")
+        @xml = HTTParty.get("http://api.amp.active.com/camping/campgrounds?landmarkName=true&landmarkLat=#{lat}&landmarkLong=#{long}&api_key=#{ENV["CAMPGROUNDKEY"]}")
         response = Hash.from_xml(@xml.body) 
         render json: response
     end
